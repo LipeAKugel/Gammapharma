16771c5d0ddfbe77881c83875ec7a850b83ddf07
João Vitor <129335105+Jauzimm@users.noreply.github.com>
2023-04-25 16:34:11 -0300

    add SistemaFarmacia
    😎

--- src/classes/SistemaFarmacia.java
+++ src/classes/SistemaFarmacia.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
 
 public class SistemaFarmacia {
-
+	
+	ArrayList<Produto> listaProdutos;
+	ArrayList<Filial> listaFiliais;
+	
+	public SistemaFarmacia () {
+		listaProdutos = new ArrayList<Produto>();
+		listaFiliais = new ArrayList<Filial>();
+	}
+	
+	public void addProduto(Produto produto) {
+		listaProdutos.add(produto);
+	}
+	
+	public void deletarProduto(Produto produto) {
+		listaProdutos.remove(produto);
+	}
+	
+	public ArrayList<Produto> listarProdutos() {
+		return listaProdutos;
+	}
+	
+	public void addFilial(Filial filial) {
+		listaFiliais.add(filial);
+	}
+	
+	public void deletarFilial(Filial filial) {
+		listaFiliais.remove(filial);
+	}
+	
+	public ArrayList<Filial> listarFiliais() {
+		return listaFiliais;
+	}
 }
