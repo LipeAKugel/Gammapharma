92d5ac1dae4f23a18702bd79bf08b189c9651c9b
Jauzimm <joaovitorso071@gmail.com>
2023-06-06 20:00:06 -0300

    Tela editar e detalhe

--- src/view/TelaDetalhe.java
+++ src/view/TelaDetalhe.java
@@ -1,24 +1,176 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TelaDetalhe {
-	TelaDetalhe() {
+public class TelaDetalhe implements ActionListener{
+	
+	private JFrame janela;
+	
+	//Variaveis Produto
+	private JLabel lnome;
+	private JTextField nome;
+	private JLabel lmarca;
+	private JTextField marca;
+	private JLabel lfabri;
+	private JTextField fabri;
+	private JLabel lpeso;
+	private JTextField peso;
+	private JLabel lvali;
+	private JTextField vali;
+	private JLabel lpreco;
+	private JTextField preco;
+	
+	//Variaveis Filiais
+	private JLabel liden;
+	private JTextField iden;
+	private JLabel llogra;
+	private JTextField logra;
+	private JLabel lcompl;
+	private JTextField compl;
+	private JLabel lcep;
+	private JTextField cep;
+	private JLabel lcidade;
+	private JTextField cidade;
+	private JLabel lestado;
+	private JTextField estado;
+	private JLabel lcnpj;
+	private JTextField cnpj;
+	
+	
+	public TelaDetalhe (int op) {
 		
 		// Crie o JFrame.
-		JFrame janela = new JFrame("Tela Detalhe");
+		janela = new JFrame("Tela Detalhe");
 		
 		// Set as bounds.
 		int j_comp = 500;
 		int j_larg = 500;
+		int p_y = 30;
+		int p_x = 30;
+		int Tp_y = 52;
 		
-		// Adicione e mostre a janela.
-		janela.setSize(j_comp, j_larg);
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		switch (op) {
+		case 1:
+			//Label
+			lnome = new JLabel("Nome: ");
+			lfabri = new JLabel("Fabricante: ");
+			lpeso = new JLabel("Peso: ");
+			lpreco = new JLabel("Preco: ");
+			lmarca = new JLabel("Marca: ");
+			lvali = new JLabel("Validade: ");
+			
+			lnome.setBounds(p_x, p_y, 60, 60);
+			lfabri.setBounds(p_x, p_y+30, 80, 60);
+			lpeso.setBounds(p_x, p_y+30*2, 60, 60);
+			lvali.setBounds(p_x, p_y+30*3, 60, 60);
+			lpreco.setBounds(p_x, p_y+30*4, 60, 60);
+			lmarca.setBounds(p_x, p_y+30*5, 60, 60);
+			
+			//JTextFields
+			
+			nome = new JTextField();
+			peso = new JTextField();
+			fabri = new JTextField();
+			vali = new JTextField();
+			preco = new JTextField();
+			marca = new JTextField();
+			
+			nome.setBounds(p_x+40, Tp_y, 200, 20);
+			fabri.setBounds(p_x+66, Tp_y+30, 200, 20);
+			peso.setBounds(p_x+35, Tp_y+30*2, 200, 20);
+			vali.setBounds(p_x+55, Tp_y+30*3, 200, 20);
+			preco.setBounds(p_x+41, Tp_y+30*4, 200, 20);
+			marca.setBounds(p_x+42, Tp_y+30*5, 200, 20);
+			
+			// Adicione e mostre a janela.
+			janela.setSize(j_comp, j_larg);
+			
+			//Adiciona JLabel
+			janela.add(lnome);
+			janela.add(lfabri);
+			janela.add(lmarca);
+			janela.add(lpeso);
+			janela.add(lpreco);
+			janela.add(lvali);
+			
+			//Adiciona JText
+			janela.add(nome);
+			janela.add(fabri);
+			janela.add(preco);
+			janela.add(marca);
+			janela.add(peso);
+			janela.add(vali);
+			//
+			janela.setLayout(null);
+			janela.setVisible(true);
+		break;
+		case 2: //label
+			liden = new JLabel("Identificador: ");
+			llogra = new JLabel("Logradouro: ");
+			lcep = new JLabel("CEP: ");
+			lcnpj = new JLabel("CNPJ: ");
+			lestado = new JLabel("Estado: ");
+			lcidade = new JLabel("Cidade: " );
+			lcompl = new JLabel("Complemento: ");
+			
+			liden.setBounds(p_x, p_y, 60, 60);
+			llogra.setBounds(p_x, p_y+30, 80, 60);
+			lcompl.setBounds(p_x, p_y+30*2, 60, 60);
+			lcep.setBounds(p_x, p_y+30*3, 60, 60);
+			lcnpj.setBounds(p_x, p_y+30*4, 60, 60);
+			lestado.setBounds(p_x, p_y+30*5, 60, 60);
+			lcidade.setBounds(p_x, p_y+30*6, 60, 60);
+			
+			//JTextFields
+			iden = new JTextField();
+			logra = new JTextField();
+			estado = new JTextField();
+			cep = new JTextField();
+			cnpj = new JTextField();
+			cidade = new JTextField();
+			compl = new JTextField();
+			
+			iden.setBounds(p_x+40, Tp_y, 200, 20);
+			logra.setBounds(p_x+66, Tp_y+30, 200, 20);
+			cep.setBounds(p_x+35, Tp_y+30*2, 200, 20);
+			cnpj.setBounds(p_x+55, Tp_y+30*3, 200, 20);
+			estado.setBounds(p_x+41, Tp_y+30*4, 200, 20);
+			cidade.setBounds(p_x+42, Tp_y+30*5, 200, 20);
+			compl.setBounds(p_x+42, Tp_y+30*6, 200, 20);
+			
+			// Adicione e mostre a janela.
+			janela.setSize(j_comp, j_larg);
+			
+			//Adiciona JLabel
+			janela.add(liden);
+			janela.add(llogra);
+			janela.add(lcep);
+			janela.add(lcnpj);
+			janela.add(lestado);
+			janela.add(lcompl);
+			janela.add(lcidade);
+			
+			//Adiciona JText
+			janela.add(iden);
+			janela.add(logra);
+			janela.add(cep);
+			janela.add(cnpj);
+			janela.add(estado);
+			janela.add(compl);
+			janela.add(cidade);
+			
+			//Tela
+			janela.setLayout(null);
+			janela.setVisible(true);
+		break;
+		}
 		
-		janela.setLayout(null);
-		janela.setVisible(true);
-		
+	}
+
+	
+	public void actionPerformed(ActionEvent e) {
+
 	}
 
 }
--- src/view/TelaEditar.java
+++ src/view/TelaEditar.java
@@ -1,28 +1,70 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TelaEditar {
+public class TelaEditar implements ActionListener {
 	private JFrame janela;
+	private JList<String> listaAmbos;
+	private static JButton cadastrarP;
+	private static JButton cadastrarF;
+	private JButton remover;
+	private String[] nomes_p = {"Paracetamol", "Dipirona", "Soro Fisiológico"};
+	private String[] nomes_f = {"Gamma Pharma Recanto", "Gamma Pharma Tagua", "Gamma Pharma Samambaia"};
 
 	public void mostrarDados(int op) {
 		
 		// Dimensões da janela.
 		int j_comp = 500;
 		int j_larg = 500;
+		int l_comp = 420;
+		int l_larg = 350;
+		
 		
 		switch (op) {
 			case 1: // Edição de produtos.
 				janela = new JFrame("Editar Produtos");
 				janela.setSize(j_comp, j_larg);
 				
+				
+				//Lista os produtos
+				listaAmbos = new JList<String>(nomes_p);
+				janela.add(listaAmbos);
+				listaAmbos.setBounds(30, 50, l_comp, l_larg);
+				
+				//Botões	
+				cadastrarP = new JButton("Cadastrar");
+				remover = new JButton("Remover");
+				cadastrarP.setBounds(120, 410, 110, 30);		
+				remover.setBounds(265, 410, 110, 30);
+				janela.add(cadastrarP);
+				janela.add(remover);
+				cadastrarP.addActionListener(this);
+				
+				//Visibilidade janela
 				janela.setLayout(null);
 				janela.setVisible(true);
+				
 			break;
 			case 2: // Edição de filiais.
 				janela = new JFrame("Editar Filiais");
 				janela.setSize(j_comp, j_larg);
 				
+				//Lista as filiais
+				listaAmbos = new JList<String>(nomes_f);
+				listaAmbos.setBounds(30, 50, l_comp, l_larg);
+				janela.add(listaAmbos);
+				
+				//Botões
+				cadastrarF = new JButton("Cadastrar");
+				remover = new JButton("Remover");
+				cadastrarF.setBounds(120, 410, 110, 30);		
+				remover.setBounds(265, 410, 110, 30);
+				janela.add(cadastrarF);
+				janela.add(remover);
+				cadastrarF.addActionListener(this);
+				
+				//Visibilidade janela
 				janela.setLayout(null);
 				janela.setVisible(true);
 			break;
@@ -30,4 +72,12 @@ public class TelaEditar {
 		
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		//Escolhe qual tela ir.
+		if(src == cadastrarP) 
+			new TelaDetalhe(1);
+		if(src == cadastrarF) 
+			new TelaDetalhe(2);
+	}
 }
